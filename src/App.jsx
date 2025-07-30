581a8d61551945f0262e861a3f0d5e02fb74d071
Abolfazl Bigdeli <im.abolfazl.bigdeli@gmail.com>
2025-07-30 10:45:16 +0330

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1 className="text-8xl font-bold">Hiii</h1>;
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-sky-900 via-slate-950 to-emerald-900 flex flex-col items-center justify-center p-4 gap-8">
+        <h1 className="text-6xl sm:text-7xl font-light bg-gradient-to-r from-emerald-400 via-sky-300 to-blue-500 bg-clip-text text-transparent text-center h-20">
+          Ai Chat App
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
